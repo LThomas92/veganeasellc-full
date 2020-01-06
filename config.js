e77ebae335ddf97183e6c6111a875147e952fc5a
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2020-01-06 02:00:34 -0500

    Update Nodemailer

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   STRIPE_SECRET_KEY: "sk_live_LjuROWOvLiAWRsxGJEvXHMcM00WGij4cML",
-  USER: "veganeasellc@gmail.com",
-  PASS: "uyorhuvvipyyjdgs"
+  USER: "lawrencegthomas@gmail.com",
+  PASS: "xmqvgbjctanblqgb"
 };
