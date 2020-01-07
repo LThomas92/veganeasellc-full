b19f70ed2a09d854c0c7fa197be2c14781d63ec3
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2020-01-06 19:10:59 -0500

    Updated PROXY

--- client/src/components/Order/MealPlanForm5.js
+++ client/src/components/Order/MealPlanForm5.js
@@ -108,7 +108,7 @@ class MealPlanForm5 extends React.Component {
 
     axios({
       method: "POST",
-      url: "http://localhost:8000/send",
+      url: "/v3/mail/send",
       data: {
         weekdayOption: weekdayOption,
         breakfastOption: breakfastOption,
